58d52677fae4db40954fc9a3b1dde82f2f1fdf02
Megan Hubbard <mah15j@my.fsu.edu>
2018-02-16 21:33:29 +0000

    Lesson 4 final commit

--- README.md
+++ README.md
@@ -4,22 +4,21 @@
 Web Application Development
 
 ### Assignment Date:  
-(Month Day, Year)
+(February 16th, 2018)
 
 ### Student Name:  
-(First and Last Name)
+(Megan Hubbard)
 
 ### Project Description:
-(In your own words, briefly describe the assignment.)
+(In this assignment we put our basic Javascript knowledge to the test by editing code for an expense calculator.)
 
 ### View Project:
-(Replace this statement with your GitHub Page URL that was created when you 
- published the project.)
+(https://meganalisa11.github.io/lesson4_javascript1/)
 
 ### Lessons Learned in the Assignment:
-1. (Briefly describe a lesson/concept learned in this lesson.)
-2. (Briefly describe a lesson/concept learned in this lesson.)
-3. (Briefly describe a lesson/concept learned in this lesson.)
+1. (Learned how to declare variables in Javascript considering that it is a loosely typed language.)
+2. (Learned different methods and elements that go along with Javascript such as parseFloat.)
+3. (Learned about the use of the clearForm method that ties together the HTML for the 'Clear Form' button with the actual action of clearing the form through Javascript)
 
 
 
--- expenseCalculator.js
+++ expenseCalculator.js
@@ -1,8 +1,8 @@
         /************************************************************************************************ 
           Course:  Web Application Development
           Assignment:  Lesson 4 (JavaScript 1)
-          Author:  
-          Date:   
+          Author:  Megan Hubbard
+          Date:  2/16/18 
           Filename: expenseCalculator.js
         *************************************************************************************************/
      
@@ -36,7 +36,12 @@
 
                   var shelterAmt = document.getElementById("shelterInput").value;
                   var foodAmt = document.getElementById("foodInput").value;
-
+                  var householdAmt = document.getElementById("householdInput").value;
+                  var miscellaneousAmt = document.getElementById("miscellaneousInput").value;
+                  var transportationAmt = document.getElementById("transportationInput").value;
+                  
+                  
+                
 
 
 
@@ -58,6 +63,9 @@
 
                   shelterAmt = parseFloat(shelterAmt);
                   foodAmt = parseFloat(foodAmt);
+                  householdAmt = parseFloat(householdAmt);
+                  transportationAmt = parseFloat(transportationAmt);
+                  miscellaneousAmt = parseFloat(miscellaneousAmt);
 
 
 
@@ -79,6 +87,9 @@
                   var totalExpenses; //total expenses for the user
                   var shelterPct; //shelter category percentage
                   var foodPct;  //food category percentage
+                  var householdPct; //household category percentage
+                  var transportationPct; //transportation category percentage
+                  var miscellaneousPct; //miscellaneous category percentage
 
 
 
@@ -107,8 +118,12 @@
                   *******************************************************************************************************/   
 
 
+                  totalExpenses = shelterAmt + foodAmt + householdAmt + transportationAmt + miscellaneousAmt; 
                   shelterPct = 100 * (shelterAmt / totalExpenses);
                   foodPct = 100 * (foodAmt / totalExpenses);
+                  householdPct = 100 * (householdAmt / totalExpenses);
+                  transportationPct = 100 * (transportationAmt / totalExpenses);
+                  miscellaneousPct = 100 * (miscellaneousAmt / totalExpenses);
 
 
                   /******************************************************************************************************
@@ -135,6 +150,9 @@
                   document.getElementById("totalExpenses").innerHTML = "<b>Total Expenses:     $</b> " + totalExpenses.toFixed(2);
                   document.getElementById("shelterPercentage").innerHTML = "% for <b>shelter</b>:      " + shelterPct.toFixed(1);      
                   document.getElementById("foodPercentage").innerHTML = "% for <b>food:</b>      " + foodPct.toFixed(1);
+                  document.getElementById("householdPercentage").innerHTML = "% for <b>household:</b>      " + householdPct.toFixed(1);
+                  document.getElementById("transportationPercentage").innerHTML = "% for <b>transportation:</b>      " + transportationPct.toFixed(1);
+                  document.getElementById("miscellaneousPercentage").innerHTML = "% for <b>miscellaneous:</b>      " + miscellaneousPct.toFixed(1);
 
 
          } //end of calculatePercentages function
@@ -171,9 +189,13 @@
                   document.getElementById("totalExpenses").innerHTML = ""; 
                   document.getElementById("shelterPercentage").innerHTML = "";      
                   document.getElementById("foodPercentage").innerHTML  = "";
+                  document.getElementById("householdPercentage").innerHTML  = "";
+                  document.getElementById("transportationPercentage").innerHTML  = "";
+                  document.getElementById("miscellaneousPercentage").innerHTML  = "";
 
 
               
+         
          }//end of clearForm function
 
 
